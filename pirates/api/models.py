8919f8a39e90f95f2358bbf5ef350aaedee32616
Thomas <thomas_kalverda@hotmail.com>
2021-09-30 15:08:59 +0200

    game and map models

--- pirates/api/models.py
+++ pirates/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+import string
+import random
 
-# Create your models here.
+def generate_unique_code():
+    length = 8
+    while True:
+        code = ''.join(random.choices(string.ascii_letters), k=length)
+        if Game.objects.filter(code=code).count() == 0:
+            break
+    return code
+
+def generate_game_name():
+    name = "NewGame"
+    return name
+
+class Map(models.Model):
+    size = models.IntegerField(null=False, default=10)
+
+class Game(models.Model):
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
+    name = models.CharField(max_length=50, null=False, default=generate_game_name)
+    number_of_players = models.IntegerField(null=False, default=8)
+    users = models.ManyToManyField(User)
+    finished = models.BooleanField(default=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    current_tick = models.IntegerField(null=False, default=0)
+    map = models.ForeignKey(Map, on_delete=models.CASCADE, related_name='games')
+
--- pirates/game/views.py
+++ pirates/game/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import *
 from .forms import CreateUserForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def registerPage(request):
@@ -11,6 +14,8 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for '+ username)
             return redirect('login')
     
     context = {'form': form}
@@ -18,4 +23,17 @@ def registerPage(request):
 
 def loginPage(request):
     context = {}
+    if request.method == 'POST':
+
+        user = authenticate(request, username=request.POST.get('username'), password=request.POST.get('password'))
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+    
     return render(request, 'game/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
