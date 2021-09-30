c7d722778352f32c1e1b21beaa408625532ca3f8
Thomas <thomas_kalverda@hotmail.com>
2021-09-30 18:50:55 +0200

    ZoomPinchPan functionality

--- pirates/frontend/src/components/App.js
+++ pirates/frontend/src/components/App.js
@@ -2,43 +2,24 @@ import React, { Component } from "react"
 import {render} from "react-dom"
 import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex, GridGenerator, HexUtils } from 'react-hexgrid';
 import configs from './configurations';
-
+import Canvas from './Canvas'
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
-        
-        const config = {
-          "width": 1920,
-          "height": 1080,
-          "layout": { "width": 6, "height": 6, "flat": false, "spacing": 1.02 },
-          "origin": { "x": 0, "y": 0 },
-          "map": "hexagon",
-          "mapProps": [ 4 ]}
-        const generator = GridGenerator.getGenerator(config.map);
-        const hexagons = generator.apply(this, config.mapProps);
-        this.state = { hexagons, config };
     }
 
     render() {
-      const { hexagons, config } = this.state;
-      const layout = config.layout;
-      const size = { x: layout.width, y: layout.height };
     return (
-      <div className="App">
-        <HexGrid width={config.width} height={config.height}>
-          <Layout size={size} flat={layout.flat} spacing={layout.spacing} origin={config.origin}>
-            {
-              // note: key must be unique between re-renders.
-              // using config.mapProps+i makes a new key when the goal template chnages.
-              hexagons.map((hex, i) => (
-                <Hexagon key={config.mapProps + i} q={hex.q} r={hex.r} s={hex.s}>
-                  {/* <Text>{HexUtils.getID(hex)}</Text> */}
-                </Hexagon>
-              ))
-            }
-          </Layout>
-        </HexGrid>
+      <div className="App" >
+        
+        
+        <TransformWrapper>
+        <TransformComponent>
+          <Canvas/>
+        </TransformComponent>
+      </TransformWrapper>
       </div>
     );
     }
---
+++ pirates/frontend/src/components/Canvas.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react"
+import {render} from "react-dom"
+import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex, GridGenerator, HexUtils } from 'react-hexgrid';
+import configs from './configurations';
+
+
+export default class Canvas extends Component {
+    constructor(props) {
+        super(props);
+        
+        const config = {
+          "width": 1800,
+          "height": 900,
+          "layout": { "width": 6, "height": 6, "flat": false, "spacing": 1.02 },
+          "origin": { "x": 0, "y": 0 },
+          "map": "hexagon",
+          "mapProps": [ 4 ]}
+        const generator = GridGenerator.getGenerator(config.map);
+        const hexagons = generator.apply(this, config.mapProps);
+        this.state = { hexagons, config };
+    }
+
+    render() {
+      const { hexagons, config } = this.state;
+      const layout = config.layout;
+      const size = { x: layout.width, y: layout.height };
+    return (
+      <div className="App">
+        <HexGrid width={config.width} height={config.height}>
+          <Layout size={size} flat={layout.flat} spacing={layout.spacing} origin={config.origin}>
+            {
+              // note: key must be unique between re-renders.
+              // using config.mapProps+i makes a new key when the goal template chnages.
+              hexagons.map((hex, i) => (
+                <Hexagon key={config.mapProps + i} q={hex.q} r={hex.r} s={hex.s}>
+                  {/* <Text>{HexUtils.getID(hex)}</Text> */}
+                </Hexagon>
+              ))
+            }
+          </Layout>
+        </HexGrid>
+      </div>
+    );
+    }
+}
